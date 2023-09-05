86a8659733b3f37c0d42cd19fa3701b0458298bb
acanxie <xie.zeyu20@gmail.com>
2023-09-05 19:58:29 +0800

    .

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const switchWord = _index => {
     _Phonetic.innerText = dic.items[_index]["Phonetic"]
 }
 
+const hideEnglish = () => {
+    _English.style.display = "none"
+}
+const hideChinese = () => {
+    _Chinese.style.display = "none"
+}
+const hidePhonetic = () => {
+    _Phonetic.style.display = "none"
+}
+const showEnglish = () => {
+    _English.style.display = "block"
+}
+const showChinese = () => {
+    _Chinese.style.display = "block"
+}
+const showPhonetic = () => {
+    _Phonetic.style.display = "block"
+}
+
 fetch("./data/GRE_words.json").then(res => res.json()).then(res => {
     dic = res
     switchWord(wordIndex)
