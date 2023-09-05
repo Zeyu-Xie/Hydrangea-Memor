aaf69c0e4ea814d33aa53244a3e7a14ff83b0eb1
acanxie <xie.zeyu20@gmail.com>
2023-09-05 19:43:19 +0800

    .

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const _English = document.getElementById("English")
 const _Chinese = document.getElementById("Chinese")
 const _Phonetic = document.getElementById("Phonetic")
+let dic = {}
+let wordIndex = 0
+
+const switchWord = _index => {
+    _English.innerText = dic.items[_index]["English"]
+    _Chinese.innerText = dic.items[_index]["Chinese"]
+    _Phonetic.innerText = dic.items[_index]["Phonetic"]
+}
 
 fetch("./data/GRE_words.json").then(res => res.json()).then(res => {
-    console.log(res)
-
-    _English.innerText = res.items[0]["English"]
-    _Chinese.innerText = res.items[0]["Chinese"]
-    _Phonetic.innerText = res.items[0]["Phonetic"]
-
-
+    dic = res
+    switchWord(1537)
 }).catch(err => {
     console.log("ERROR", err)
 })
